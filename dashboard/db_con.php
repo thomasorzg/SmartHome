ca1f5915d22e143ec0004e9956eb78a1b76c04ba
Thomas <jaapagamerz12@gmail.com>
2021-08-24 01:12:11 -0700

    Actualización de valores de la conexión a la base de datos

--- dashboard/db_con.php
+++ dashboard/db_con.php
@@ -14,5 +14,8 @@ en Universidad Tecnólogica del Sur de Sonora.
 */
 
 // Establecer conexión directa al gestor de base de datos
-$db_con = mysqli_connect("localhost", "root", 'tommyo24', "smarthome");
+// $db_con = mysqli_connect("localhost", "root", 'tommyo24', "smarthome"); Local
+
+// Utilizando RemoteMySQL para la aplicación web hosteada en Heroku
+$db_con = mysqli_connect("remotemysql.com", "ZQi7Yoe8Gv", 'zottbwGx8G', "ZQi7Yoe8Gv");
 ?>
